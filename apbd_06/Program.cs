b41e2b3bcd1ecf1658c61488bfc9cbc94630a019
JakubDrozdz <jakubekso@outlook.com>
2024-06-08 13:01:59 +0200

    PR to main | implement-app (#1)
    * Implements model classes
    
    * Db migration
    
    * Model adjustments for controller
    
    * Implements patient check
    
    * Implement medicament check
    
    * Update employee check
    
    * Implements DueDate check
    
    * Implements adding prescription
    
    * Updates Prescription model
    
    * Implements fetching patient details
    
    * Adds custom exceptions
    
    * Adds doctor ID to create prescription command

--- apbd_06/Program.cs
+++ apbd_06/Program.cs
@@ -1,9 +1,20 @@
+using apbd_06;
+using apbd_06.Repositiories;
+using apbd_06.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-//builder.Services.AddScoped<, >();
+builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+builder.Services.AddScoped<IPrescriptionRepository, StandardPrescriptionRepository>();
+builder.Services.AddScoped<IPatientRepository, StandardPatientRepository>();
+builder.Services.AddScoped<IMedicamentRepository, StandardMedicamentRepository>();
+builder.Services.AddScoped<IPrescriptionMedicamentRepository, PrescriptionMedicamentRepository>();
+builder.Services.AddScoped<MainDbContext, MainDbContext>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IDoctorRepository, StandardDoctorRepository>();
 
 var app = builder.Build();
 
